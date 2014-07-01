f90446d00c9cec41264f092b45ea6ff85af5c3db
Oskar Zabik <oskar.zabik@gmail.com>
2014-07-01 18:16:36 +0200

    getting data from Gmail and updating app view

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ var pages = {
 var inputWon  = document.getElementById('won')
 var inputLost = document.getElementById('lost');
 
+var lostCount;
+var wonCount;
+
 // hide preloader, goto signin page
 document.querySelector('body').classList.add('active')
 window.location.hash = pages.signin;
@@ -43,5 +46,67 @@ inputWon.addEventListener('keydown', function(e) {
   if (e.keyCode === 13) {
     localStorage.setItem('won-label', e.target.value)
     window.location.hash = pages.app;
+    getGmailData()
   };
 })
+
+
+// When on App page get Gmail data
+var getGmailData = function() {
+
+	// show page preloader 
+  document.body.classList.remove('active');
+
+	gapi.client.load('gmail', 'v1', function(e) {
+		var gmail = gapi.client.gmail;
+
+		// get lost leads
+		var requestLostEmails = gmail.users.threads.list({
+			userId: 'me',
+			q: 'label:' + localStorage.getItem('lost-label')
+		})
+
+		requestLostEmails.execute(function(response) {
+			// set counter
+			lostCount = response.threads ? response.threads.length : 0;
+
+			// if other request is already finished
+			// then update view
+			if (wonCount !== undefined) {
+				updateView();
+			};
+		})
+
+		// get won leads
+		var requestWonEmails = gmail.users.threads.list({
+			userId: 'me',
+			q: 'label:' + localStorage.getItem('won-label')
+		})
+
+		requestWonEmails.execute(function(response) {
+			// set counter
+			wonCount = response.threads ? response.threads.length : 0;
+
+			if (lostCount !== undefined) {
+				updateView();
+			};
+		})
+
+	})
+}
+
+
+// All the data is ready, update app view
+var updateView = function() {
+	// get app component
+	var app = document.querySelector('gmail-conversions');
+
+	// update component view
+	app.won = wonCount;
+	app.lost = lostCount;
+	app.wonLabel = localStorage.getItem('won-label');
+	app.lostLabel = localStorage.getItem('lost-label');
+
+	// hide preloader
+	document.body.classList.add('active');
+}
